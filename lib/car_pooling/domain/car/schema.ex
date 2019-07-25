6e0ef4bfefeeeb3684694561bdecdb03aa8f42d2
Kamil Lelonek <squixy.sln@gmail.com>
2019-07-25 15:09:05 +0200

    Extend Car Schema

--- lib/car_pooling/domain/car/schema.ex
+++ lib/car_pooling/domain/car/schema.ex
@@ -1,8 +1,12 @@
 defmodule CarPooling.Domain.Car.Schema do
   use Ecto.Schema
 
+  alias CarPooling.Domain.Journey.Schema, as: Journey
+
   schema "cars" do
     field :seats, :integer
+
+    has_one(:journey, Journey, foreign_key: :id)
 
     timestamps(updated_at: false)
   end
