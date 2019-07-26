09c00471ac98c6cc2beddf38022fa32fe6912341
Kamil Lelonek <squixy.sln@gmail.com>
2019-07-26 16:16:58 +0200

    Configure test DB for GitLab

--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,10 @@
 use Mix.Config
 
 config :car_pooling, CarPooling.Domain.Repo,
-  database: "car_pooling_test",
+  username: System.get_env("POSTGRES_USER") || "postgres",
+  password: System.get_env("POSTGRES_PASSWORD") || "postgres",
+  database: System.get_env("POSTGRES_DB") || "car_pooling_test",
+  hostname: System.get_env("POSTGRES_HOST") || "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :car_pooling, CarPoolingWeb.Endpoint,
